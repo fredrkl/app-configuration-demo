d6a62114657f635fbc9731ba35550dc4330d0146
Fredrik Klingenberg <fredrkl@gmail.com>
2025-01-08 21:38:42 +0100

    ✨ Basic configuration management

---
+++ src/Program.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var builder = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string?>()
+{
+    ["key1"] = "value1",
+    ["key2"] = "value2"
+});
+
+IConfiguration configuration = builder.Build();
+
+Console.WriteLine(configuration["key1"]);
