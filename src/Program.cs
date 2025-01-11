60c643853b316d6e27a208ae66078374e2e9ea79
Fredrik Klingenberg <fredrkl@gmail.com>
2025-01-11 09:02:03 +0100

    ⚗️ Simple start

--- src/Program.cs
+++ src/Program.cs
@@ -1,10 +1,11 @@
 ﻿using Microsoft.Extensions.Configuration;
 
-var builder = new ConfigurationBuilder().AddInMemoryCollection(new Dictionary<string, string?>()
-{
+var builder = new ConfigurationBuilder()
+.AddInMemoryCollection(new Dictionary<string, string?>()
+    {
     ["key1"] = "value1",
     ["key2"] = "value2"
-});
+    });
 
 IConfiguration configuration = builder.Build();
 
