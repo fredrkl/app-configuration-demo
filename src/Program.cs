3eb2b91eccf108be58899a02c1d4a16234963e63
Fredrik Klingenberg <fredrkl@gmail.com>
2025-04-01 12:39:40 +0200

    ✨ Using Client Secret Credential

--- src/Program.cs
+++ src/Program.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Azure.Identity;
 
+ClientSecretCredential credential = new ClientSecretCredential(Environment.GetEnvironmentVariable("AZURE_TENANT_ID"), Environment.GetEnvironmentVariable("AZURE_CLIENT_ID"),Environment.GetEnvironmentVariable("AZURE_CLIENT_SECRET"));
+
 var builder = new ConfigurationBuilder()
   .SetBasePath(Directory.GetCurrentDirectory())
   .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
   .AddAzureAppConfiguration(options => {
       options.Connect(Environment.GetEnvironmentVariable("AZURE_APP_CONFIGURATION_CONNECTION_STRING"));
       options.ConfigureKeyVault(kv => {
-          kv.SetCredential(new DefaultAzureCredential());
+          kv.SetCredential(credential);
       });
     })
   .AddInMemoryCollection(new Dictionary<string, string?>()
